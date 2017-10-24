fdd5600a216570c2d76c3727602f237b47fc8c9e
lh <lh_1215077166@sina.cn>
2017-10-24 20:33:35 +0800

    shrio基础应用

---
+++ myshrio-base/src/test/java/org/myshrio/base/AppTest.java
@@ -0,0 +1,26 @@
+package org.myshrio.base;
+
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.util.Factory;
+
+import junit.framework.TestCase;
+public class AppTest  extends TestCase{
+	public void testApp(){
+		Factory<org.apache.shiro.mgt.SecurityManager> factory=new IniSecurityManagerFactory("classpath:shrio.ini");
+
+		org.apache.shiro.mgt.SecurityManager manager=factory.getInstance();
+		SecurityUtils.setSecurityManager(manager);
+
+		org.apache.shiro.subject.Subject subject=SecurityUtils.getSubject();
+
+		AuthenticationToken authenticationToken=new UsernamePasswordToken("mldn", "123456");
+		subject.login(authenticationToken);
+		System.out.println("用户名:"+authenticationToken.getPrincipal());
+		System.out.println("密码："+authenticationToken.getCredentials().toString());
+
+	}
+}
